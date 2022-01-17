b691ae1f8869f690eb0347d384104f9ab569bf99
Maizares, Juan <juan_maizares386@hotmail.com>
2022-01-17 12:37:23 -0300

    nota

--- src/components/01-useState/CounterApp.js
+++ src/components/01-useState/CounterApp.js
@@ -9,6 +9,7 @@ export const CounterApp = () => {
     return (
         <>
             <h1>Probando el useState</h1>
+            <h3>Temos un hook, useState, que manipula un objeto (contador 1 y 2). Con el seteador incrementamos un solo contador.</h3>
             <h1>Contador1: { counter1 }</h1>
             <h1>Contador2: { counter2 }</h1>
             <hr />
