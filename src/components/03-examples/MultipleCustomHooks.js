1d8375096db3dd6f56d8db7f01d52bc051ef90c0
Maizares, Juan <juan_maizares386@hotmail.com>
2022-01-24 12:17:37 -0300

    useFetch con useCounter hook para consumir API de quotes

--- src/components/03-examples/MultipleCustomHooks.js
+++ src/components/03-examples/MultipleCustomHooks.js
@@ -1,15 +1,38 @@
 import React from 'react';
+import { useCounter } from '../../hooks/useCounter';
 import { useFetch } from '../../hooks/useFetch';
 import '../02-useEffect/effects.css'
 
 export const MultipleCustomHooks = () => {
-  const state = useFetch(`https://breakingbadapi.com/api/quotes/1 `)
-  console.log(state)
+
+  const {counter, increment} = useCounter(1)
+  const {loading, data} = useFetch(`https://breakingbadapi.com/api/quotes/${counter} `)
+  const {author, quote} = !!data && data[0]
+
   return (
       <div>
           <h1>
-              Custom Hooks !!!
-          </h1>
+              Breaking Bad API 
+          </h1><hr />
+          {
+              loading
+              ?
+                (
+                    <div className='alert alert-info text-center'>
+                        Loading...
+                    </div>    
+                )
+              :
+                (
+                    <blockquote className='blockquote text-right'>
+                        <p className='mb-3'>{quote}</p>
+                        <footer className='blockquote-footer'>{author}</footer>
+                    </blockquote>
+                )
+          }
+          <button className='btn btn-primary' onClick={increment}>Siguiente ...</button>
+          
+          
       </div>
   )
 };
